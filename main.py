2a48e9c32db1e99e68d4e8ce475bd637ae91e8c8
Sanwar Hussain <work.sanwar.hussain@gmail.com>
2025-07-14 01:57:56 +0530

    cors domain addition

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ allowed_origins = [
     "http://localhost:3000",
     "http://localhost:8000",  
     "http://127.0.0.1:3000",
-    "https://medassistyou.vercel.app",
+    "https://medassistapp.vercel.app",
 ]
 
 app.add_middleware(
